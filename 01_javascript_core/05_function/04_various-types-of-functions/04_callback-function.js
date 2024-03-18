81d377c5a55fd025a3a73f4f7ce7f021e5960627
alu98@naver.com <alu98@naver.com>
2024-03-18 11:50:28 +0900

    콜백 함수 작성완료

---
+++ 01_javascript_core/05_function/04_various-types-of-functions/04_callback-function.js
@@ -0,0 +1,25 @@
+/* 04.callback-function(콜백 함수) */
+
+/* 전달 받은 값을 1 증가시켜 반환하는함수 */
+function increase(value) {
+    return value + 1;
+}
+
+/* 전달 받은 값을 1 감소시켜 반환하는 함수 */
+function decrease(value) {
+    return value - 1;
+}
+
+/* 전달 받은 함수에 전달 받은 값을 적용시켜 주는 고차 함수(전달 받은 함수를 활용하는 함수) */
+function apply(func, value) {
+    return func(value);
+}
+
+console.log(apply(increase, 5));        // increase 함수를 콜백 함수 ,6
+console.log(apply(decrease, 5));        // decrease 함수를 콜백 함수 ,4
+// increase 나 decrease 함수를 받아 활용하는 apply는 고차 함수
+
+/* 배열의 정렬을 다룰 때의 예시 */
+console.log([1, 2, 3, 4, 5].sort(function(left, right) {return right - left}));         // [5, 4, 3, 2, 1]
+
+/* 비동기 처리(이벤트 처리, 타이버,ajax, fetch, ...)에 활용되는 중요한 패턴이다. */
