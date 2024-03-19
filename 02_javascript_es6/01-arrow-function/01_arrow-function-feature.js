0b72ee2d0273f29899462ec11fd3ab6740e0761b
alu98@naver.com <alu98@naver.com>
2024-03-19 11:49:53 +0900

    arrow function feature 작성중 commit

--- 02_javascript_es6/01-arrow-function/01_arrow-function-feature.js
+++ 02_javascript_es6/01-arrow-function/01_arrow-function-feature.js
@@ -1,1 +1,21 @@
 /* 01. arrow-function-feature */
+
+let theater = {
+    store: "킨텍스점",
+    titles: ["파묘", "듄2", "가여운 것들", "포켓몬스터"],
+
+    showMovieList() {       // showMovieList: function() => {} ES6부터는 생략가능
+        console.log(this.store);
+
+        // this.titles.forEach(title => 
+        //     console.log(this.store + ": " + title));
+
+        this.titles.forEach(
+            function(title) {
+                console.log(this.store + ": " + title);
+            }
+        )
+    }
+}
+
+theater.showMovieList();            // 킨텍스점
