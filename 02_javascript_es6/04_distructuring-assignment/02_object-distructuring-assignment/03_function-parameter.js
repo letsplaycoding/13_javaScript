e0167c81e58df8db63adfbe199ca8248227ab7d5
alu98@naver.com <alu98@naver.com>
2024-03-19 15:28:01 +0900

    commit

--- 02_javascript_es6/04_distructuring-assignment/02_object-distructuring-assignment/03_function-parameter.js
+++ 02_javascript_es6/04_distructuring-assignment/02_object-distructuring-assignment/03_function-parameter.js
@@ -1,1 +1,18 @@
-/* 03. function-parameter */
+/* 03. function-parameter(함수의 매개변수에서 구조 분해 할당) */
+
+let exampleProduct = {
+    items: ["Coffee", "Soju"],
+    producer: "한국인"
+};
+
+function displayProduct({producer = "김이박", items = [], width = 10, height = 20}) {
+    console.log(producer);
+    console.log(items);
+    console.log(width);
+    console.log(height);
+}
+
+displayProduct(exampleProduct);
+
+/* 함수 호출 시 객체를 던지면 내부적으로 매개변수에 적힌 구조분해 할당이 일어난다. */
+// let {producer = "김이박", items = [], width = 10, height = 20} = exampleProduct;
