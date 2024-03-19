8e5210c308fd594543cefc638eaf6884c4b5779b
alu98@naver.com <alu98@naver.com>
2024-03-19 10:20:37 +0900

    Math-method 수정

--- 01_javascript_core/10_standard-built-in-object/02_Math/02_Math-method.js
+++ 01_javascript_core/10_standard-built-in-object/02_Math/02_Math-method.js
@@ -1,6 +1,7 @@
 /* 02. Math-method */
 
 // Math.abs
+console.log('----------ABS----------');
 console.log(Math.abs(-10));                     
 console.log(Math.abs('-10'));                   
 console.log(Math.abs(''));                     
@@ -10,71 +11,71 @@ console.log(Math.abs(undefined));
 console.log(Math.abs({}));                      
 console.log(Math.abs('math'));                 
 console.log(Math.abs());                       
-console.log('--------------------');
 
 // Math.round
+console.log('----------ROUND----------');
 console.log(Math.round(10.1));                  
 console.log(Math.round(10.9));                  
 console.log(Math.round(-10.1));                 
 console.log(Math.round(-10.9));                 
 console.log(Math.round(10));                    
 console.log(Math.round());                      
-console.log('--------------------');
 
 // Math.ceil
+console.log('----------CEIL----------');
 console.log(Math.ceil(10.1));                 
 console.log(Math.ceil(10.9));                 
 console.log(Math.ceil(-10.1));                
 console.log(Math.ceil(-10.9));                
 console.log(Math.ceil(10));                   
 console.log(Math.ceil());                     
-console.log('--------------------');
 
 // Math.floor
+console.log('----------FLOOR----------');
 console.log(Math.floor(10.1));                
 console.log(Math.floor(10.9));                
 console.log(Math.floor(-10.1));               
 console.log(Math.floor(-10.9));               
 console.log(Math.floor(10));                  
 console.log(Math.floor());                    
-console.log('--------------------');
 
 // Math.sqrt
+console.log('----------SQRT----------');
 console.log(Math.sqrt(4));                    
 console.log(Math.sqrt(-4));                   
 console.log(Math.sqrt(2));                    
 console.log(Math.sqrt(1));                    
 console.log(Math.sqrt(0));                    
 console.log(Math.sqrt());                     
-console.log('--------------------');
 
 // Math.random
+console.log('----------RANDOM----------');
 console.log(Math.random());               
 // 1~100 범위의 난수 추출       
 const random = Math.floor((Math.random() * 100) + 1);
 console.log(random);
-console.log('--------------------');
 
 // Math.pow
+console.log('----------POW----------');
 console.log(Math.pow(2, 2));                 
 console.log(Math.pow(2, -2));                
 console.log(Math.pow(2));                    
 
 /* ES7에서 도입 된 지수 연산자를 사용할 수 있다. */
+console.log('----------지수 연산자----------');
 console.log(2 ** 2);                         
 console.log(2 ** -2);                        
-console.log('--------------------');
 
 // Math.max
+console.log('----------MAX----------');
 console.log(Math.max(10));                   
 console.log(Math.max(10, 20));               
 console.log(Math.max(10, 20, 30));           
 console.log(Math.max());                     
-console.log('--------------------');          
 
 // Math.min
+console.log('----------MIN----------');          
 console.log(Math.min(10));                   
 console.log(Math.min(10, 20));               
 console.log(Math.min(10, 20, 30));           
-console.log(Math.min());                     
-console.log('--------------------');    
+console.log(Math.min());                       
